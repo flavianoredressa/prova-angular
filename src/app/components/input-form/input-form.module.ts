dbd64b34272cce522a4163c60ff82d43c5c328be
Flaviano_Redressa <flavianoredressa@hotmail.com>
2020-05-06 10:20:30 -0300

    Add Mask

--- src/app/components/input-form/input-form.module.ts
+++ src/app/components/input-form/input-form.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { InputFormComponent } from './input-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
-// import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
